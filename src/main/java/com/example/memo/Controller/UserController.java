7ad030cb9c70934e1076086ea167265687a061da
Bhanuprakash Chinta <chintabhanuprakashreddy@gmail.com>
2025-05-26 12:53:57 +0530

    New endpoint

--- src/main/java/com/example/memo/Controller/UserController.java
+++ src/main/java/com/example/memo/Controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -77,6 +78,13 @@ public class UserController {
         return new ResponseEntity<>(userService.getUser(id),HttpStatus.ACCEPTED);
     }
     
+    @GetMapping("/getUserForOrder/{token}")
+    public ResponseEntity<UserDto> getUserForOrder(@PathVariable String token)
+    {
+        String id=jwtToken.extractUserId(token);
+        return new ResponseEntity<>(userService.getUser(id),HttpStatus.ACCEPTED);
+    }
+
 
     @GetMapping("/validateUser")
     public ResponseEntity<?> validateRegistredUser(@RequestParam String token)
